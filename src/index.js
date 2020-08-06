936c67976dd90ce04681d66c9f180421c46dc373
Mayur <mayurnegi1999@gmail.com>
2020-08-06 09:18:00 +0530

    fetched movies from api and applied thunk

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
 import "./index.css";
 import App from "./components/App";
 import rootReducers from "./reducers";
 
-const store = createStore(rootReducers);
+// function looger(obj,next, action );
+// logger(obj)(next)(action)
+// const logger = function ({ dispatch, getState }) {
+//   return function (next) {   // next is calling the nxt middleware in chain or dispatch if we are at last middleware
+//     return function (action) {
+//       console.log("ACTION_TYPE= ", action.type);
+//       next(action);
+//     };
+//   };
+// };
+
+const logger = ({ dispatch, getState }) => (next) => (action) => {
+  if (typeof action !== "function") {
+    console.log("ACTION_TYPE= ", action.type);
+  }
+  next(action);
+};
+
+// const thunk = ({ dispatch, getState }) => (next) => (action) => {
+//   if (typeof action === "function") {
+//     action(dispatch);
+//     return;
+//   }
+//   next(action);
+// };
+
+const store = createStore(rootReducers, applyMiddleware(logger, thunk));
 
 // console.log("store", store);
 // console.log("before state", store.getState());
