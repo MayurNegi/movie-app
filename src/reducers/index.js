46b678a70afbd560fcb4a697256722a5778238ab
Mayur <mayurnegi1999@gmail.com>
2020-08-06 09:47:58 +0530

    adding search result in store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,6 +5,8 @@ import {
   ADD_TO_FAVOURITES,
   REMOVE_FROM_FAVOURITES,
   SET_SHOW_FAVOURITES,
+  ADD_SEARCH_RESULT,
+  ADD_MOVIE_TO_LIST,
 } from "../actions/index";
 
 // set initial movie state
@@ -47,17 +49,39 @@ export function movies(state = initialMovieState, action) {
         showFavourites: action.val,
       };
 
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        list: [action.movie, ...state.list],
+      };
+
     default:
       return state;
   }
 }
 
 const initialSearchState = {
+  showSearchResults: false,
   result: {},
 };
 export function search(state = initialSearchState, action) {
-  console.log("SEARCH REDUCER");
-  return state;
+  switch (action.type) {
+    case ADD_SEARCH_RESULT:
+      return {
+        ...state,
+        result: action.movie,
+        showSearchResults: true,
+      };
+
+    case ADD_MOVIE_TO_LIST:
+      return {
+        ...state,
+        showSearchResults: false,
+      };
+
+    default:
+      return state;
+  }
 }
 
 const initialRootState = {
